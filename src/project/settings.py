1ef3712eed80a220457a3ec0eb94e77b1f4b7757
Mohamed Sherif <mohamedsherif2395@gmail.com>
2021-12-14 00:48:24 +0200

    is_employer default value

--- src/project/settings.py
+++ src/project/settings.py
@@ -91,9 +91,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES={
    'default':{
       'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME':'grad_project',
+      'NAME':'test',
       'USER':'postgres',
-      'PASSWORD':'mesho3010',
+      'PASSWORD':'12457896532',
       'HOST':'localhost',
       'PORT':'5432',
    }
