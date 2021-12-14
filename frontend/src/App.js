52707e21208ab9f179dde4ef0897006d1277791a
MohamedElsmahy <mohamedtelsmahy@gmail.com>
2021-12-14 14:28:50 +0200

    add login and signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import JobsList from "./pages/JobsList";
 import JobDetails from "./pages/JobDetails";
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
 import Header from "./components/Header";
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="jobs/" element={<JobsList />} />
           <Route path="jobs/:id" element={<JobDetails />} />
+          <Route path="signup/" element={<SignUp />} />
+          <Route path="signin/" element={<SignIn /> } />
         </Routes>
       </Router>
     </div>
